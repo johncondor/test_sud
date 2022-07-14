7f4a3bafc413e66946dcbfd93a15e29d71e21a3d
perlowmaps <107708394+perlowmaps@users.noreply.github.com>
2022-07-14 12:31:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -450,7 +450,7 @@ map.on("load", function () {
             data: geojsonData,
           },
           paint: {
-            "circle-radius": 5, // size of circles
+            "circle-radius": 8, // size of circles
             "circle-color": "#3D2E5D", // color of circles
             "circle-stroke-color": "white",
             "circle-stroke-width": 1,
